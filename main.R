e65cd305345a428aaebda6578f1d7da5f6cb677a
Truman Fogler <tfogler@scc2.bu.edu>
2024-05-08 17:43:29 -0400

    Update Report, Main
    Update report text to using HISAT2
    Add first function to main
    and import packages

--- main.R
+++ main.R
@@ -1,1 +1,33 @@
 # main.R
+
+#Imports
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.16")
+# Bioconductor version '3.19' requires R version '4.4'; 
+# use `BiocManager::install(version = '3.16')` with R version '4.2'
+BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
+
+library(tidyverse)
+library(DESeq2)
+
+#' Load a tsv located at specific location `filename` into a tibble
+#'
+#'
+#' @param filename (str): the path to a specific file (ie 'file/path/to/file.tsv')
+#'
+#' @return tibble: a (g x 1+m) tibble with a 'gene' column followed by
+#' sample names as column names.
+#'
+#' @note Column 'gene' should be first and the only column to contain strings.
+#' Data in sample_name columns CANNOT be strings
+#'
+#' @example `verse_counts <- read_data('verse_counts.tsv')`
+
+read_data <- function(filename){
+
+  return (read_tsv(filename))
+}
+
+
