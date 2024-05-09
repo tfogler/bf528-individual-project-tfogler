c0700e0a50d899a431b61944374df3bee1c91fcb
Truman Fogler <tfogler@bu.edu>
2024-05-09 17:15:26 -0400

    PCA plot and histogram

--- main.R
+++ main.R
@@ -10,7 +10,7 @@ if (!require("BiocManager", quietly = TRUE))
 # BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
 # BiocManager::install(c("DESeq2", "fgsea"))
 
-libs <- c("tidyverse", "BiocManager", "DESeq2", "fgsea")
+libs <- c("tidyverse", "BiocManager", "DESeq2", "fgsea", "biomaRt", "RColorBrewer")
 
 for (package in libs) {
   suppressPackageStartupMessages(require(package, 
@@ -34,6 +34,22 @@ for (package in libs) {
 load_n_trim <- function(filename, ..., row.names) {
   f <- read.delim(filename, row.names=row.names)
   f <- f[,c("CTL_rep1", "CTL_rep2", "CTL_rep3", "KO_rep1", "KO_rep2", "KO_rep3")]
+  return(f)
+}
+
+#' Read Data
+#' Remove all zero variance genes
+#' 
+#' @param filename full file path as a string of the counts file 
+#'
+#' @examples counts_df <- read_data("/path/to/counts/verse_counts.tsv")
+read_data <- function(filename, ...) {
+  f <- read_tsv(filename)
+  var <- apply(f[-1],
+                         MARGIN = 1,
+                         FUN = var,
+                         na.rm = F)
+  f <- f[var > 0,]
   return(f)
 }
 
@@ -76,5 +92,141 @@ run_deseq <- function(count_dataframe, coldata, count_filter, condition_name) {
   # run differential analysis
   dds <- DESeq(dds, quiet=T)
   
-  results(dds)
+  return(dds)
+}
+
+
+#' Perform and plot PCA using processed data.
+#'
+#' PCA is performed over genes, and samples should be colored by time point.
+#' Both `y` and `x` axis should have percent of explained variance included.
+#'
+#'
+#' @param data tibble: a (n x _S_) data set
+#' @param meta tibble: sample-level meta information (_S_ x 3)
+#' @param title string: title for plot
+#'
+#' @return ggplot: scatter plot showing each sample in the first two PCs.
+#'
+#' @examples
+#' `plot_pca(data, meta, "Raw Count PCA")`
+
+plot_pca <- function(dds, title="") {
+  # get normalized counts
+  norm_counts <- counts(dds, normalized = TRUE)
+  # Perform PCA
+  pca_result <- prcomp(t(norm_counts), scale = TRUE)
+  # Principal components (loadings)
+  principal_components <- pca_result$rotation
+  # Data in principal component space (scores)
+  scores <- pca_result$x
+  
+  pca_df <- as.data.frame(pca_result$x)
+  pca_df$Sample <- rownames(pca_df)  # Add sample names as a column
+  
+  ggplot(pca_df,
+        mapping=aes(`PC1`, `PC2`,
+                    colour = `Sample`,
+                    label = `Sample`
+        )
+  ) +
+    scale_color_brewer(palette = 'Set2') +
+    geom_point(size=6) +
+    labs(title = title,
+         x = "Principal Component 1",
+         y = "Principal Component 2") %>% 
+  return(pc_plot)
+}
+
+#' Acquire gene list
+#' 
+acquire_gene_list <- function(df) {
+  mart <- biomaRt::useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+  genes <- df$genes
+  df$id <- NA
+  G_list <- biomaRt::getBM(filters= "ensembl_gene_id", attributes= c("ensembl_gene_id",
+                                                            "entrezgene", "description"),values=genes,mart= mart)
+  return(G_list)
+}
+
+
+#' Function to generate a named vector ranked by log2FC descending
+#'
+#' @param labeled_results (tibble): Tibble with DESeq2 results and one
+#'   additional column denoting status in volcano plot
+#' @param id2gene_path (str): Path to the file containing the mapping of
+#' ensembl IDs to MGI symbols
+#'
+#' @return Named vector with gene symbols as names, and log2FoldChange as values
+#' ranked in descending order
+#' @export
+#'
+#' @examples rnk_list <- make_ranked_log2fc(labeled_results, 'data/id2gene.txt')
+
+make_ranked_log2fc <- function(labeled_results, id2gene_path) {
+  # make ranked list for fgsea
+  # start with reading id2gene list
+  id2gene <- read_delim(file=id2gene_path, delim='\t', col_names=c('ensembl_id', 'gene_symbol'))
+  # assign a joined tibble with mgi genes ids to labeled_res & sort by log2FC descending
+  full_join(labeled_results, id2gene, by=join_by(gene == ensembl_id)) %>%
+    arrange(desc(log2FoldChange)) %>%
+    filter(!is.na(log2FoldChange)) -> labeled_results
+  # subset out the l2FC col and add names from mgi symbols b4 returning
+  labeled_results$log2FoldChange %>% `names<-`(labeled_results$gene_symbol) %>%
+    return()
+}
+
+#' Function to run fgsea with arguments for min and max gene set size
+#'
+#' @param gmt_file_path (str): Path to the gene sets of interest in GMT format
+#' @param rnk_list (named vector): Named vector generated previously with gene 
+#' symbols and log2Fold Change values in descending order
+#' @param min_size (int): Minimum number of genes in gene sets to be allowed
+#' @param max_size (int): Maximum number of genes in gene sets to be allowed
+#'
+#' @return Tibble of results from running fgsea
+#' @export
+#'
+#' @examples fgsea_results <- run_fgsea('data/m2.cp.v2023.1.Mm.symbols.gmt', rnk_list, 15, 500)
+run_fgsea <- function(gmt_file_path, rnk_list, min_size, max_size) {
+  pathways <- gmtPathways(gmt_file_path)
+  fgsea(pathways, rnk_list, min_size, max_size) %>%
+    return()
+}
+
+#' Function to display fgsea results as tibble sorted by padj
+#' (ascending)
+#' 
+#' 
+#' @examples display_fgsea(fgsea_results)
+display_fgsea <- function(fgsea_results) {
+  fgsea_results %>%
+    arrange(padj) %>%
+    head(n=10L) %>%
+    return()
+}
+
+#' Function to plot top ten positive NES and top ten negative NES pathways
+#' in a barchart
+#'
+#' @param fgsea_results (tibble): the fgsea results in tibble format returned by
+#'   the previous function
+#' @param num_paths (int): the number of pathways for each direction (top or
+#'   down) to include in the plot. Set this at 10.
+#'
+#' @return ggplot with a barchart showing the top twenty pathways ranked by positive
+#' and negative NES
+#' @export
+#'
+#' @examples fgsea_plot <- top_pathways(fgsea_results, 10)
+top_pathways <- function(fgsea_results, num_paths){
+  top_fgsea_results <- fgsea_results %>%
+    arrange(NES) %>%
+    dplyr::slice(c(1:num_paths, (n()-num_paths):n()))
+  `levels<-`(top_fgsea_results$NES, top_fgsea_results$NES)
+  ggplot(top_fgsea_results, mapping=aes(`NES`, substr(`pathway`, 10, 19))) +
+    geom_col(aes(fill = `NES` > 0)) +
+    # scale_color_manual(values=c("#0000ff", "red")) +
+    labs(title="fgsea results for Hallmark MSigDB gene pathways") %>%
+    return()
 }
